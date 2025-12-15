4460f252cb5e7c0dffae7e3019ee2a6461c92178
Jinfeng Wang <jinfeng.wang@razer.com>
2025-12-15 14:33:16 +0800

    1

---
+++ src/ArgoTest/ArgoTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArgoTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public String Get()
+        {
+            return "V1";
+            
+        }
+    }
+}
