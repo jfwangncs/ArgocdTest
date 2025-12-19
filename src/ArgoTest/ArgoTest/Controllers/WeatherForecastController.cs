1da1d58d8e98e16291fd4e23334d2bf8bcdd2bab
Jinfeng Wang <jinfeng.wang@razer.com>
2025-12-19 10:01:04 +0800

    v24

--- src/ArgoTest/ArgoTest/Controllers/WeatherForecastController.cs
+++ src/ArgoTest/ArgoTest/Controllers/WeatherForecastController.cs
@@ -17,14 +17,44 @@ namespace ArgoTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public async Task<String> Get()
         {
-            var version = "V23";
+            var version = "V24";
             var startTime = DateTime.UtcNow;
             await Task.Delay(new Random().Next(1000));
             var endTime = DateTime.UtcNow;
             var duration = (endTime - startTime).TotalMilliseconds;
-            _logger.LogInformation("Version:{Version},Log:{Log},Duration:{Duration}", version, "测试", duration);
+            var org = new Organization() { Name = "RG", Tag = new List<string>() { "a", "B", "D" } };
+            var user = new User()
+            {
+                Id = 23,
+                Name = "希望",
+                Organization = org,
+                Team = new List<Team>() { new Team { Organization = org, Tag = ["x", "y", "z"] },
+                new Team { Organization = new Organization() { Name = "WB",            Tag = new List<string>() { "o", "跑", "大打" } }, Tag = ["！@#", "大家", "大打"] } }
+            };
+            _logger.LogInformation("Version:{Version},Log:{Log},Duration:{Duration},User:{User}", version, "测试", duration, user);
             return version;
 
         }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Organization Organization { get; set; }
+        public List<Team> Team { get; set; }
+    }
+
+    public class Organization
+    {
+        public string Name { get; set; }
+
+        public List<string> Tag { get; set; }
+    }
+    public class Team
+    {
+        public Organization Organization { get; set; }
+        public List<string> Tag { get; set; }
     }
 }
